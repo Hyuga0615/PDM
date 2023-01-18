abc25cc332974ecaa7f57955d00428e7aa7f4f71
Hyuga0615 <hyuga5884@gmail.com>
2023-01-19 02:44:53 +0900

    chousei

--- app/Models/Sale.php
+++ app/Models/Sale.php
@@ -9,15 +9,4 @@ use Illuminate\Database\Eloquent\Model;
 class Sale extends Model
 {
     use HasFactory;
-    
-    protected $fillable = [
-        'name',
-        'name_kana',
-        'mail_address',
-        'password',
-        'zip_code',
-        'address',
-        'phone_number',
-        'birthday'
-        ];
 }
